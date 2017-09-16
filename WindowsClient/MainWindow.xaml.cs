0ccf2d9742a75494bc82ae2aeefde8aa861f5b6a
Benjamin Hamon <hamon.benjamin@gmail.com>
2017-09-16 14:24:05 +0200

    Add initial project

---
+++ WindowsClient/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Overmind.ImageManager.WindowsClient
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ WindowsClient/WindowsApplication.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+
+namespace Overmind.ImageManager.WindowsClient
+{
+	public partial class WindowsApplication : System.Windows.Application
+	{
+		[STAThread]
+		public static void Main(string[] arguments)
+		{
+			Model.Application application = new Model.Application();
+			WindowsApplication windowsApplication = new WindowsApplication(application);
+			windowsApplication.InitializeComponent();
+			windowsApplication.Run();
+		}
+
+		public WindowsApplication(Model.Application application)
+		{
+			this.application = application;
+		}
+
+		private readonly Model.Application application;
+
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+			MainWindow = new MainWindow();
+			MainWindow.Show();
+		}
+	}
+}

1f3fb280a7b9f67772e5a6d5333bfa3b25ff12b1
Benjamin Hamon <hamon.benjamin@gmail.com>
2017-09-16 16:06:32 +0200

    Add DelegateCommand for MVVM

---
+++ WindowsClient/DelegateCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Overmind.ImageManager.WindowsClient
+{
+	/// <summary>
+	/// ICommand implementation for MVVM.
+	/// See http://wpftutorial.net/DelegateCommand.html
+	/// </summary>
+	/// <typeparam name="TParameter">Type of parameter passed to the command, use object if there is none.</typeparam>
+	public class DelegateCommand<TParameter> : ICommand
+	{
+		public DelegateCommand(Action<TParameter> execute, Predicate<TParameter> canExecute = null)
+		{
+			this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+			this.canExecute = canExecute;
+		}
+
+		private readonly Action<TParameter> execute;
+		private readonly Predicate<TParameter> canExecute;
+		public event EventHandler CanExecuteChanged;
+
+		public void Execute(object parameter) { execute((TParameter)parameter); }
+		public bool CanExecute(object parameter) { return canExecute == null ? true : canExecute((TParameter)parameter); }
+		public void RaiseCanExecuteChanged() { CanExecuteChanged?.Invoke(this, EventArgs.Empty); }
+	}
+}

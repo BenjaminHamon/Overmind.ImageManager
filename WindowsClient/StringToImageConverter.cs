5e6e94de6a98d64c04634900226a0e79009d66af
Benjamin Hamon <hamon.benjamin@gmail.com>
2017-09-23 20:34:08 +0200

    Fix image files staying opened by the application

---
+++ WindowsClient/StringToImageConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Overmind.ImageManager.WindowsClient
+{
+	// This converter avoids holding a lock on the image file.
+	[ValueConversion(typeof(string), typeof(ImageSource))]
+	public class StringToImageConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			string filePath = value as string;
+			if ((filePath != null) && File.Exists(filePath))
+			{
+				BitmapImage image = new BitmapImage();
+				using (FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+				{
+					image.BeginInit();
+					image.CacheOption = BitmapCacheOption.OnLoad;
+					image.StreamSource = stream;
+					image.EndInit();
+				}
+				return image;
+			}
+			return null;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}

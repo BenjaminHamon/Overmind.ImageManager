975e0b6e9322b22a7a4ab3731bab8cc099d74ab1
Benjamin Hamon <hamon.benjamin@gmail.com>
2018-12-02 12:38:52 +0100

    Ensure data model is updated before saving or closing

--- WindowsClient/MainView.xaml.cs
+++ WindowsClient/MainView.xaml.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Input;
 using WPFCustomMessageBox;
 
 namespace Overmind.ImageManager.WindowsClient
@@ -59,10 +60,18 @@ namespace Overmind.ImageManager.WindowsClient
 			viewModel.LoadCollectionCommand.Execute(Path.GetDirectoryName(fileDialog.FileName));
 		}
 
+		private void SaveCollection(object sender, EventArgs eventArguments)
+		{
+			ForceUpdateOnFocusedElement();
+			viewModel.SaveCollectionCommand.Execute(null);
+		}
+
 		private void CloseCollection(object sender, EventArgs eventArguments)
 		{
 			if (viewModel.ActiveCollection == null)
 				return;
+
+			ForceUpdateOnFocusedElement();
 
 			if (viewModel.ActiveCollection.IsSaved())
 			{
@@ -165,6 +174,27 @@ namespace Overmind.ImageManager.WindowsClient
 				if (downloaderWindow.WindowState == WindowState.Minimized)
 					downloaderWindow.WindowState = WindowState.Normal;
 				downloaderWindow.Activate();
+			}
+		}
+
+		private void ForceUpdateOnFocusedElement()
+		{
+			// Some controls update their source when they lose focus, but this does not happen when switching focus scope.
+			// The issue occurs when using a menu command or closing the window (directly or from the system task bar).
+
+			// See also https://stackoverflow.com/questions/57493/wpf-databind-before-saving
+			// - Disabling the menu focus scope is only a partial fix since it does not catch the issue when the main window is closed,
+			//   plus it introduces a bug where the main window close button may require two clicks, from the focus being stuck on the menu somehow
+			//   (the behavior is similar to having a menu open, the first click would dismiss the menu and does not trigger on the button)
+			// - Updating the active element directly relies on Keyboard.FocusedElement, which is null when closing the window from the system task bar,
+			//   and requires supporting any control type (TokenListView does not expose its internal update logic).
+
+			Window mainWindow = Window.GetWindow(this);
+			IInputElement focusedElement = FocusManager.GetFocusedElement(mainWindow);
+			if (focusedElement != null)
+			{
+				FocusManager.SetFocusedElement(mainWindow, this);
+				FocusManager.SetFocusedElement(mainWindow, focusedElement);
 			}
 		}
 	}

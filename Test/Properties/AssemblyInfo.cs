b995b7d01db14d08c38c887c9cc5849fc4e905cf
Benjamin Hamon <hamon.benjamin@gmail.com>
2019-04-23 16:10:14 +0200

    Add unit test project

---
+++ Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Overmind.ImageManager.Test")]
+[assembly: AssemblyDescription("")]
+
+[assembly: ComVisible(false)]

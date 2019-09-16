e03301f153817c5a5a53637107124e87d24c598a
Benjamin Hamon <hamon.benjamin@gmail.com>
2019-09-16 13:31:29 +0200

    Change file name formatter to allow most unicode characters and add unit tests

--- Model/FileNameFormatter.cs
+++ Model/FileNameFormatter.cs
@@ -8,15 +8,17 @@ namespace Overmind.ImageManager.Model
 {
 	public class FileNameFormatter
 	{
-		public string AllowedAlphabet = "a-zA-Z0-9";
+		private static readonly Regex InvalidCharactersRegex = new Regex("[" + new string(Path.GetInvalidFileNameChars()) + "]", RegexOptions.Compiled);
+		private static readonly Regex WhiteSpaceRegex = new Regex(@"\s+", RegexOptions.Compiled);
+
 		public string WordSeparator { get; set; } = " ";
 		public string ValueSeparator { get; set; } = ", ";
 		public string ElementSeparator { get; set; } = " - ";
-		public string EllipsisMark { get; set; } = " ...";
+		public string EllipsisMark { get; set; } = "...";
 		public string TitleDefaultValue { get; set; } = "NoTitle";
 		public string ArtistDefaultValue { get; set; } = "NoArtist";
-		public int TitleSizeLimit { get; set; } = 50;
-		public int ArtistSizeLimit { get; set; } = 30;
+		public int TitleLengthLimit { get; set; } = 50;
+		public int ArtistLengthLimit { get; set; } = 30;
 		public Func<string, string> TextTransform { get; set; }
 
 		public string Format(ImageModel image, string fileExtension = null)
@@ -24,33 +26,35 @@ namespace Overmind.ImageManager.Model
 			if (String.IsNullOrEmpty(fileExtension))
 				fileExtension = Path.GetExtension(image.FileName).TrimStart('.');
 
-			string titleElement = FormatElement(new List<string>() { image.Title }, TitleSizeLimit)
-				?? FormatElement(image.SubjectCollection, TitleSizeLimit) ?? TitleDefaultValue;
-			string artistElement = FormatElement(image.ArtistCollection, ArtistSizeLimit) ?? ArtistDefaultValue;
+			string titleElement = FormatElement(new List<string>() { image.Title }, TitleLengthLimit)
+				?? FormatElement(image.SubjectCollection, TitleLengthLimit) ?? TitleDefaultValue;
+			string artistElement = FormatElement(image.ArtistCollection, ArtistLengthLimit) ?? ArtistDefaultValue;
 			List<string> allElements = new List<string>() { titleElement, artistElement, image.Hash };
 			return String.Join(ElementSeparator, allElements) + "." + fileExtension;
 		}
 
-		private string FormatElement(IEnumerable<string> valueCollection, int sizeLimit)
+		public string FormatElement(IEnumerable<string> valueCollection, int lengthLimit)
 		{
 			string fullValue = String.Join(ValueSeparator, valueCollection.Select(FormatValue));
 			if (String.IsNullOrEmpty(fullValue))
 				return null;
 
+			lengthLimit -= EllipsisMark.Length;
+
 			string shortenedValue = fullValue;
-			while ((shortenedValue.Length > sizeLimit) && (shortenedValue.Contains(",")))
+			while ((shortenedValue.Length > lengthLimit) && (shortenedValue.Contains(",")))
 				shortenedValue = shortenedValue.Substring(0, shortenedValue.LastIndexOf(","));
-			while ((shortenedValue.Length > sizeLimit) && (shortenedValue.Contains(" ")))
+			while ((shortenedValue.Length > lengthLimit) && (shortenedValue.Contains(" ")))
 				shortenedValue = shortenedValue.Substring(0, shortenedValue.LastIndexOf(" "));
-			if (shortenedValue.Length > sizeLimit)
-				shortenedValue = shortenedValue.Substring(0, sizeLimit);
+			if (shortenedValue.Length > lengthLimit)
+				shortenedValue = shortenedValue.Substring(0, lengthLimit);
 
 			if (shortenedValue.Length < fullValue.Length)
 				return shortenedValue + EllipsisMark;
 			return shortenedValue;
 		}
 
-		private string FormatValue(string value)
+		public string FormatValue(string value)
 		{
 			if (value == null)
 				return null;
@@ -58,8 +62,8 @@ namespace Overmind.ImageManager.Model
 			if (TextTransform != null)
 				value = TextTransform(value);
 
-			value = Regex.Replace(value, "[^" + AllowedAlphabet + "]", WordSeparator);
-			value = Regex.Replace(value, WordSeparator + "+", WordSeparator);
+			value = InvalidCharactersRegex.Replace(value, "");
+			value = WhiteSpaceRegex.Replace(value, " ");
 			return value.Trim();
 		}
 	}

f131b1cb1ca239a2305a695f9e52e3f4733aa8bb
Benjamin Hamon <hamon.benjamin@gmail.com>
2020-05-27 11:56:46 +0200

    Upgrade development toolkit dependency to 032434c526e880fe1672f59ab4992f4b883ddbff

--- development/configuration.py
+++ development/configuration.py
@@ -19,7 +19,7 @@ def load_configuration(environment):
 	configuration["copyright"] = "Copyright (c) 2020 Benjamin Hamon"
 
 	configuration["development_toolkit"] = "git+https://github.com/BenjaminHamon/DevelopmentToolkit@{revision}#subdirectory=toolkit"
-	configuration["development_toolkit_revision"] = "a4ad1edfb956641c420d42ff369087cffb6d7584"
+	configuration["development_toolkit_revision"] = "032434c526e880fe1672f59ab4992f4b883ddbff"
 	configuration["development_dependencies"] = [ "pylint" ]
 
 	configuration["dotnet_solution"] = "Overmind.ImageManager.sln"

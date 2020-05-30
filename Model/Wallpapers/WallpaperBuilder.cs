2764312cd632267b898860b027a5e1866692748c
Benjamin Hamon <hamon.benjamin@gmail.com>
2020-05-30 23:18:40 +0200

    Move WallpaperBuilder to model

--- Model/Wallpapers/WallpaperBuilder.cs
+++ Model/Wallpapers/WallpaperBuilder.cs
@@ -3,7 +3,7 @@ using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.Linq;
 
-namespace Overmind.ImageManager.WallpaperService
+namespace Overmind.ImageManager.Model.Wallpapers
 {
 	public class WallpaperBuilder
 	{

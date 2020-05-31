cfdbd551d54184864e2ac5e712f766dcc53370aa
Benjamin Hamon <hamon.benjamin@gmail.com>
2020-05-31 19:29:07 +0200

    Add title bar control

---
+++ WpfExtensions/Controls/TitleBar.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Overmind.WpfExtensions.Controls
+{
+	public class TitleBar : ContentControl
+	{
+		public static readonly DependencyProperty IconProperty
+			= DependencyProperty.Register(nameof(Icon), typeof(FrameworkElement), typeof(TitleBar));
+		public static readonly DependencyProperty MenuProperty
+			= DependencyProperty.Register(nameof(Menu), typeof(FrameworkElement), typeof(TitleBar));
+		public static readonly DependencyProperty TitleProperty
+			= DependencyProperty.Register(nameof(Title), typeof(FrameworkElement), typeof(TitleBar));
+
+		public FrameworkElement Icon
+		{
+			get { return (FrameworkElement)GetValue(IconProperty); }
+			set { SetValue(IconProperty, value); }
+		}
+
+		public FrameworkElement Menu
+		{
+			get { return (FrameworkElement)GetValue(MenuProperty); }
+			set { SetValue(MenuProperty, value); }
+		}
+
+		public FrameworkElement Title
+		{
+			get { return (FrameworkElement)GetValue(TitleProperty); }
+			set { SetValue(TitleProperty, value); }
+		}
+	}
+}
